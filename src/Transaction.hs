ce8faff3a09c58dd58869f7b758248307f994d68
Lars Brünjes <brunjlar@gmail.com>
2021-04-04 20:19:36 +0200

    output port

--- src/Transaction.hs
+++ src/Transaction.hs
@@ -1,3 +1,15 @@
 module Transaction where
 
+import Graphics.SvgTree
+import Linear.V2
 import Reanimate
+import Reanimate.Raster
+
+test :: IO ()
+test = reanimate $ staticFrame 0.1 $ txOutput 0.07 0.1
+
+txOutput :: Double -> Double -> Tree
+txOutput r1 r2 = withStrokeWidth (r2 - r1)
+               $ withStrokeColor "red"
+               $ withFillOpacity 0
+               $ mkCircle $ (r1 + r2) / 2
