37980c4f190871d525e2bc8fe0489040365afd8b
Lars Brünjes <brunjlar@gmail.com>
2021-04-05 00:21:31 +0200

    transaction and input animation

--- src/Transaction.hs
+++ src/Transaction.hs
@@ -2,7 +2,7 @@
 
 module Transaction where
 
-import Data.Text        (Text)
+import Data.Text        (Text, pack)
 import Graphics.SvgTree hiding (Text)
 import Linear.V2
 import Reanimate
@@ -15,7 +15,10 @@ scenario =
     staticFrame 0 (mkBackground "white") `andThen`
     ( output "Alice" "100 ₳" "Datum" (-9, 1) (-6, 3) `parA`
       output "Bob"   "100 ₳" "Datum" (-9, 0) (-5, -3)
-    )
+    ) `andThen`
+    tx 1 (-3, 1) `andThen`
+    input "redeemer" (-3, 1) (-6, 3) `andThen`
+    pause 1
 
 outputPort :: Tree
 outputPort = withStrokeWidth 0.03
@@ -44,11 +47,53 @@ output addr amt dat (x1, y1) (x2, y2) =
           $ withStrokeColor "red"
           $ withFillOpacity 0
           $ mkPath
-                [ MoveTo OriginAbsolute [V2 x1 y1]
-                , CurveTo OriginAbsolute [(V2 (x1 + 1) y1, V2 (x2 - 2) y2, V2 (x2 - 1) y2)]
+                [ MoveTo OriginAbsolute [V2 (x1 + 0.35) y1]
+                , CurveTo OriginAbsolute [(V2 (x1 + 1.35) y1, V2 (x2 - 2) y2, V2 (x2 - 1) y2)]
                 , LineTo OriginAbsolute [V2 (x2 - 0.1) y2]
                 ]
     b = animate $ \s -> translate x2 y2 $ partialSvg s $ pathify outputPort
 
     t :: Tree -> Animation
     t = staticFrame 0.15 . translate x2 y2
+
+tx :: Int -> (Double, Double) -> Animation
+tx i (x, y) = setDuration 0.2 a `andThen` staticFrame 0.8 t
+  where
+    a :: Animation
+    a = animate $ \s -> partialSvg s $ pathify
+            $ withStrokeColor "black"
+            $ withFillColor "gray"
+            $ translate x y
+            $ mkRect 0.7 2
+
+    t :: Tree
+    t = translate x (y - 0.05) $ text $ pack $ "Tx " ++ show i
+
+input :: Text -> (Double, Double) -> (Double, Double) -> Animation
+input red (x1, y1) (x2, y2) =
+    setDuration 0.6 a `andThen`
+    setDuration 0.2 b `andThen`
+    staticFrame 0.2 c
+  where
+    a, b :: Animation
+    a =  animate $ \s ->
+            partialSvg s
+          $ withStrokeWidth 0.03
+          $ withStrokeColor "blue"
+          $ withFillOpacity 0
+          $ mkPath
+                [ MoveTo OriginAbsolute [V2 (x1 - 0.35) y1]
+                , CurveTo OriginAbsolute [(V2 (x1 - 1.15) y1, V2 (x2 + 2) y2, V2 (x2 + 1) y2)]
+                , LineTo OriginAbsolute [V2 x2 y2]
+                ]
+
+    b = animate $ \s ->
+              withFillColor "blue"
+            $ withStrokeWidth 0
+            $ translate x2 y2
+            $ partialSvg s
+            $ pathify
+            $ mkCircle 0.05
+
+    c :: Tree
+    c = translate (x2 + 0.5) (y2 + 0.1) $ text red
