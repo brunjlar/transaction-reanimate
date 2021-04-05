1d7de0f1ae397c085cdb4f4b95de59b3e8c303f1
Lars Brünjes <brunjlar@gmail.com>
2021-04-05 21:56:22 +0200

    switched to latex

--- src/Transaction.hs
+++ src/Transaction.hs
@@ -12,22 +12,22 @@ import Reanimate
 transactions :: IO ()
 transactions = reanimate $ a0 `andThen` (a1 `seqA` a2 `seqA` a3 `seqA` a4 `seqA` a5 `seqA` a6)
 
-scenario :: Text -> Text -> Animation
-scenario x y =
-    ( output x       "100 ₳" y  (-10, 1) (-6, 3) `parA`
-      output "Bob"    "50 ₳" "" (-10, 0) (-4, -4)
-    )                                                   `andThen`
-    tx 1 (-3, 1)                                        `andThen`
-    input "" (-3, 1) (-6, 3)                            `andThen`
-    output "Bob"   "10 ₳" "" (-3, 0.5) (0, -2)          `andThen`
-    output "Alice" "90 ₳" "" (-3, 1.5) (-0.5, 2)        `andThen`
-    tx 2 (2, -2)                                        `andThen`
-    input "" (2, -1.5) (-0.5, 2)                        `andThen`
-    input "" (2, -2) (0, -2)                            `andThen`
-    input "" (2, -2.5) (-4, -4)                         `andThen`
-    output "Alice"    "35 ₳" "" (2, -1.5) (6, 3.5)      `andThen`
-    output "Bob"       "5 ₳" "" (2, -2)   (7, 1.5)      `andThen`
-    output "Charlie" "110 ₳" "" (2, -2.5) (6.5, -4)     `andThen`
+scenario :: Text -> Text -> Text -> Animation
+scenario x y z =
+    ( output x       "100 ada" y  (-10, 1) (-6, 3) `parA`
+      output "Bob"    "50 ada" "" (-10, 0) (-4, -4)
+    )                                                     `andThen`
+    tx 1 (-3, 1)                                          `andThen`
+    input z (-3, 1) (-6, 3)                               `andThen`
+    output "Bob"   "10 ada" "" (-3, 0.5) (0, -2)          `andThen`
+    output "Alice" "90 ada" "" (-3, 1.5) (-0.5, 2)        `andThen`
+    tx 2 (2, -2)                                          `andThen`
+    input "" (2, -1.5) (-0.5, 2)                          `andThen`
+    input "" (2, -2) (0, -2)                              `andThen`
+    input "" (2, -2.5) (-4, -4)                           `andThen`
+    output "Alice"    "35 ada" "" (2, -1.5) (6, 3.5)      `andThen`
+    output "Bob"       "5 ada" "" (2, -2)   (7, 1.5)      `andThen`
+    output "Charlie" "110 ada" "" (2, -2.5) (6.5, -4)     `andThen`
     pause 1
 
 a0, a1, a2, a3, a4, a5, a6 :: Animation
@@ -36,13 +36,13 @@ a0 = staticFrame 0 $ mkGroup
         , withFillOpacity 0.8 $ mkBackground "white"
         ]
 
-a1 = scenario "Alice" ""
+a1 = scenario "Alice" "" ""
 
 a2 = animate (\t -> scale (1 + 2.1 * t * t) $ translate (6 * t) (-3 * t) svg) `andThen` pause 1
   where
     svg = lastFrame a1
 
-a3 = lastFrame a2 `fade`scale 3.1 (translate 6 (-3) $ lastFrame $ scenario "Script" "") `andThen` pause 1
+a3 = lastFrame a2 `fade`scale 3.1 (translate 6 (-3) $ lastFrame $ scenario "Script" "" "Redeemer") `andThen` pause 1
 
 a4 = animate (\t -> scale (3.1 - 2.1 * t) $ translate ((-6) * t) (3 * t) svg) `andThen` pause 1
   where
@@ -52,7 +52,7 @@ a5 = animate (\t -> scale (1 + 0.55 * t) $ translate (3.6 * t) (- 0.6 * t) svg) 
   where
     svg = lastFrame a4
 
-a6 = lastFrame a5 `fade`scale 1.55 (translate 3.6 (-0.6) $ lastFrame $ scenario "Script" "Datum") `andThen` pause 1
+a6 = lastFrame a5 `fade`scale 1.55 (translate 3.6 (-0.6) $ lastFrame $ scenario "Script" "Datum" "Redeemer") `andThen` pause 1
 
 lastFrame :: Animation -> Tree
 lastFrame a = frameAt (duration a) a
@@ -67,18 +67,19 @@ outputPort = withStrokeWidth 0.03
            $ mkCircle 0.085
 
 text :: Text -> Tree
-text = scale 0.1
+text = scale 0.2
+     . center
      . withFillColor "black"
      . withStrokeColor "black"
-     . mkText
+     . latex
 
 output :: Text -> Text -> Text -> (Double, Double) -> (Double, Double) -> Animation
 output addr amt dat (x1, y1) (x2, y2) =
-    setDuration 0.4  a                      `andThen`
-    setDuration 0.15 b                      `andThen`
-    t (translate (-0.5)   0.1  $ text addr) `andThen`
-    t (translate (-0.5) (-0.2) $ text amt)  `andThen`
-    t (translate (-0.5) (-0.4) $ text dat)
+    setDuration 0.4  a                       `andThen`
+    setDuration 0.15 b                       `andThen`
+    t (translate (-0.5)   0.15  $ text addr) `andThen`
+    t (translate (-0.5) (-0.15) $ text amt)  `andThen`
+    t (translate (-0.5) (-0.35) $ text dat)
   where
     a, b :: Animation
     a = animate $ \s ->
@@ -136,4 +137,4 @@ input red (x1, y1) (x2, y2) =
             $ mkCircle 0.05
 
     c :: Tree
-    c = translate (x2 + 0.5) (y2 + 0.1) $ text red
+    c = translate (x2 + 0.55) (y2 + 0.15) $ text red
