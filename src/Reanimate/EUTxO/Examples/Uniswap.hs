23ec7244d78692b548f967b2d1c02fa8bb977cd7
Lars Brünjes <brunjlar@gmail.com>
2021-06-07 13:43:45 +0200

    background image and animation start

--- src/Reanimate/EUTxO/Examples/Uniswap.hs
+++ src/Reanimate/EUTxO/Examples/Uniswap.hs
@@ -4,12 +4,31 @@ module Reanimate.EUTxO.Examples.Uniswap
     ( uniswap
     ) where
 
-import Control.Monad                    (void)
+import Reanimate
 
 import Reanimate.EUTxO.Monadic.Advanced
 
 uniswap :: IO ()
-uniswap = reanimateM $ do
+uniswap = reanimate $ andThen background $ runAnimationM $ do
+    tx1       <- txM True (-7.5, 0)
+    oFactory1 <- outputM "Factory" "US NFT" (Just "[]") tx1 (-6, 3)
+    delayM
+    tx2       <- txM False (-10, 3)
+    oAlice1   <- outputM "Alice" "1000A+2000B" Nothing tx2 (-5, 4)
+    delayM
+    tx3       <- txM True (-3.2, 3)
+    inputM Nothing oAlice1 tx3
+    inputM (Just "Create") oFactory1 tx3
+    oAlice2   <- outputM "Alice" "1414AB" Nothing tx3 (-1, 2)
+    oFactory2 <- outputM "Factory" "US NFT" (Just "[AB]") tx3 (-2, 0)
+    delayM
+
+background :: Animation
+background = staticFrame 0 $ mkGroup
+    [ mkImage 16 9 "stock-exchange.jpg"
+    , withFillOpacity 0.8 $ mkBackground "white"
+    ]
+{-
     txGen   <- txM False (-10, 0)
     oAlice1 <- outputM "Alice" "100 ada" Nothing txGen (-5, 0)
     delayM
@@ -18,3 +37,4 @@ uniswap = reanimateM $ do
     void $ outputM "Bob" "10 ada" Nothing tx (3, 2)
     void $ outputM "Alice" "90 ada" Nothing tx (4, -1)
     delayM
+-}
